78a75843e72c69b13cfabd101e0c25efa6655440
Ashley Williams <ashley.williams@strongmind.com>
2025-02-09 16:55:12 -0700

    add login view add signup view fix css

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password
+    };
+
+    fetch('https://filmapi-ab3ce15dfb3f.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.user) {
+          localStorage.setItem('user', JSON.stringify(data.user));
+          localStorage.setItem('token', data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert('No such user');
+        }
+      })
+      .catch((e) => {
+        alert('Something went wrong');
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="login-form">
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+          className="form-input"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="form-input"
+        />
+      </label>
+      <button type="submit" className="submit-button">Submit</button>
+    </form>
+  );
+};
+
+LoginView.propTypes = {
+  onLoggedIn: PropTypes.func.isRequired,
+};
---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+export const SignupView = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    };
+
+    fetch('https://filmapi-ab3ce15dfb3f.herokuapp.com/users', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      if (response.ok) {
+        alert('Signup successful');
+        window.location.reload();
+      } else {
+        alert('Signup failed');
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="signup-form">
+      <h2>Sign Up</h2>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+          className="form-input"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          className="form-input"
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="form-input"
+        />
+      </label>
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required
+          className="form-input"
+        />
+      </label>
+      <button type="submit" className="submit-button">Submit</button>
+    </form>
+  );
+};
