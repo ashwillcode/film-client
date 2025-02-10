d42fddcca17d4ea79a900272fc0f78647d776792
Ashley Williams <ashley.williams@strongmind.com>
2025-02-09 19:13:37 -0700

    added bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 import './index.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => <MainView />;
 
